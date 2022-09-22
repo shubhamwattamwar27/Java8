95dbd049f8b06888182fd6785e46b5e6ca26be30
shubham.wattamwar <shubham.wattamwar27@gmail.com>
2022-09-22 11:11:04 +0530

    Method Reference Example and Stream Example

--- src/Stream/StreamVsCollectionExample.java
+++ src/Stream/StreamVsCollectionExample.java
@@ -28,6 +28,12 @@ public class StreamVsCollectionExample {
         streamName.forEach(System.out::println);
         //streamName.forEach(System.out::println); // you will get exception because you can not
         // use stream multiple times
+        /*
+        * Exception in thread "main" java.lang.IllegalStateException: stream has already been operated upon or closed
+	    at java.util.stream.AbstractPipeline.sourceStageSpliterator(AbstractPipeline.java:279)
+	    at java.util.stream.ReferencePipeline$Head.forEach(ReferencePipeline.java:580)
+	    at Stream.StreamVsCollectionExample.main(StreamVsCollectionExample.java:29)
+        * */
 
     }
 
