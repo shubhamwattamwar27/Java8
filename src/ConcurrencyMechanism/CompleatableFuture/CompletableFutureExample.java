c3990e9d4b0cac2e4e861887ef12efd334a82f00
shubham.wattamwar <shubham.wattamwar27@gmail.com>
2022-09-26 21:28:46 +0530

    Completable Future

--- src/ConcurrencyMechanism/CompleatableFuture/CompletableFutureExample.java
+++ src/ConcurrencyMechanism/CompleatableFuture/CompletableFutureExample.java
@@ -2,6 +2,8 @@ package ConcurrencyMechanism.CompleatableFuture;
 
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Supplier;
 
 public class CompletableFutureExample {
@@ -10,10 +12,26 @@ public class CompletableFutureExample {
 
         Supplier<Integer> supplier = () -> new Random().nextInt();
         for(int i = 0 ;i< 5; i++){
+            // internally it uses ForkJoinPool
             CompletableFuture.supplyAsync(() -> supplier.get() + supplier.get())
                     .thenApply(o -> Integer.max(supplier.get(),o))
+                    .exceptionally(throwable -> 12)
                     .thenApplyAsync(o -> Integer.min(supplier.get(),o))
                     .thenAccept(integer -> System.out.println(integer));
         }
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService ioExecutorService = Executors.newCachedThreadPool();
+
+        for(int i = 0 ;i< 5; i++){
+            // we can specify our thread pool as well -- for async we can execute with diff diff threads
+            CompletableFuture.supplyAsync(() -> supplier.get() + supplier.get(),executorService)
+                    .thenApply(o -> Integer.max(supplier.get(),o))
+                    .thenApplyAsync(o -> Integer.min(supplier.get(),o),ioExecutorService)
+                    .thenAcceptAsync(integer -> System.out.println(integer),executorService);
+        }
+
+
     }
 }
